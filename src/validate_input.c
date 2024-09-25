80a4f154f6241121c2663af23bbd7aed19eebb56
SzymonSuchanek <jus20be20+1@gmail.com>
2024-09-25 16:31:04 +0200

    update validation

--- src/validate_input.c
+++ src/validate_input.c
@@ -6,71 +6,46 @@
 /*   By: ssuchane <ssuchane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 18:15:21 by ssuchane          #+#    #+#             */
-/*   Updated: 2024/09/24 18:46:43 by ssuchane         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:28:57 by ssuchane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-// >>, >, <<, < at the end of the strng
-// | at the beginning and the end of the string
-// unclosed quotes or double quotes
-// if unclosed double quotes are within single quotes the input is correct
-// if unclosed single quotes are within double quotes the input is correct
-// everything except $ within double quotes is treated as string literal
-
-bool	is_valid_input(const char *input)
+bool	is_input_valid(const char *input)
 {
-	bool single_quote_open = false;
-	bool double_quote_open = false;
-	size_t len = strlen(input);
+	int		i;
+	char	quote;
+	char	last;
+	int		len;
 
-	// Check if input ends with valid redirection operator
-	if (len >= 2 && (strcmp(input + len - 2, ">>") == 0 || strcmp(input + len
-				- 2, "<<") == 0))
+	len = strlen(input);
+	last = input[len - 1];
+	if (input[0] == '|' || last == '|' || last == '>' || last == '<')
+		return (false);
+	i = 0;
+	while (input[i])
 	{
-		len -= 2; // Reduce length to check the rest of the input
-	}
-	else if (len >= 1 && (input[len - 1] == '>' || input[len - 1] == '<'))
-	{
-		len -= 1; // Reduce length to check the rest of the input
-	}
-
-	// Check if input starts and ends with pipe
-	if (input[0] == '|')
-	{
-		if (len > 0 && input[len - 1] == '|')
+		if (input[i] == '\'' || input[i] == '"')
 		{
-			len--; // Ignore the ending pipe
+			quote = input[i];
+			i++;
+			while (input[i] && input[i] != quote)
+				i++;
+			if (!input[i])
+				return (false);
 		}
-		else
-		{
-			return (false);
-				// Invalid if it starts with | but doesn't end with it
-		}
+		i++;
 	}
-
-	// Iterate through the input string to check quotes
-	for (size_t i = 0; i < len; i++)
-	{
-		if (input[i] == '\'')
-		{
-			single_quote_open = !single_quote_open;
-				// Toggle single quote status
-		}
-		else if (input[i] == '\"')
-		{
-			double_quote_open = !double_quote_open;
-				// Toggle double quote status
-		}
-	}
-
-	// At the end, quotes must be closed or in acceptable open states
-	if ((single_quote_open && !double_quote_open) || (double_quote_open
-			&& !single_quote_open))
-	{
-		return (false); // One of the quotes is unclosed
-	}
-
-	return (true); // All conditions met
+	return (true);
 }
+
+// int	main(void)
+// {
+// 	char *input = "t34rvwerv\"dsacrawcer\"sdfsdf 'r4af44'|";
+// 	int	i;
+
+// 	i = is_input_valid(input);
+// 	printf("%d\n", i);
+// 	return (0);
+// }
